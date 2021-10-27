422f1553974c2bb37bb5e231c94006e5770d1873
aisha1 <aishafadallala@gmail.com>
2021-10-27 07:59:03 +0300

    going

--- coffeehub/coffeehub/settings.py
+++ coffeehub/coffeehub/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for coffeehub project.
 
@@ -128,3 +130,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
